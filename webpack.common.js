973cd83befcf986fee97d5cc8b87201d39cbf148
Yakobus Octavianus Sucahyo Putra <yakobusavin2004@gmail.com>
2025-11-10 17:54:10 +0700

    third commit

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,4 @@
+// webpack.common.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -9,24 +10,36 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true, // membersihkan dist sebelum build baru
   },
   module: {
     rules: [
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext][query]', // simpan gambar di folder dist/images/
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'], // jika kamu punya file CSS
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html'),
+      template: path.resolve(__dirname, 'src/public/index.html'),
+      filename: 'index.html',
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+          globOptions: {
+            ignore: ['**/index.html'], // jangan salin index.html lagi (sudah ditangani HtmlWebpackPlugin)
+          },
         },
       ],
     }),
